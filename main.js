3a56ed3bf3c9e9466a47d1ca24e62fbf8aeb9762
Cameron Alexander <emptyflash@gmail.com>
2022-12-03 13:09:56 -0500

    Fix rendering speed

--- main.js
+++ main.js
@@ -4,43 +4,30 @@ import {defaultKeymap} from "@codemirror/commands"
 import {javascript} from "@codemirror/lang-javascript"
 
 
+
 const canvas = document.getElementById("canvas");
 canvas.width = Math.floor(window.innerWidth);
 canvas.height = Math.floor(window.innerHeight);
 const width = canvas.width;
 const height = canvas.height;
-const ctx = canvas.getContext("2d");
+const context = canvas.getContext("webgl2", { preserveDrawingBuffer: true });
 
-const gpu = new GPU();
+const gpu = new GPU({canvas, context});
 let kernel;
+
 function evalCode(code) {
   const codeFn = Function("t", `
     let x = this.thread.x;
     let y = this.thread.y;
-    return ${code};
+    let result = ${code}
+    result /= 255;
+    this.color(result, result, result, 1);
   `);
-  kernel = gpu.createKernel(codeFn).setOutput([width, height]);
+  kernel = gpu.createKernel(codeFn).setOutput([width, height]).setGraphical(true);
   params.set("c", code);
   window.history.replaceState({}, '', `${location.pathname}?${params.toString()}`);
   return true;
 }
-
-function render(time) {
-  const outputBufferRaw = kernel(time);
-  const outputBuffer = new Uint8ClampedArray(width*height*4);
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width*4; x++) {
-      outputBuffer[x*4+y*width*4] = outputBufferRaw[y][x];
-      outputBuffer[x*4+y*width*4+1] = outputBufferRaw[y][x];
-      outputBuffer[x*4+y*width*4+2] = outputBufferRaw[y][x];
-      outputBuffer[x*4+y*width*4+3] = 255;
-    }
-  }
-  const output = new ImageData(outputBuffer, width, height);
-  ctx.putImageData(output, 0, 0);
-  requestAnimationFrame(render)
-}
-requestAnimationFrame(render);
 
 const params = new URLSearchParams(location.search);
 if (params.get("c")) {
@@ -68,3 +55,11 @@ function runButtonFn() {
 }
 const runButton = document.getElementById("runButton");
 runButton.onclick = runButtonFn
+
+function render(time) {
+  if (kernel) {
+    kernel(time);
+  }
+  requestAnimationFrame(render)
+}
+requestAnimationFrame(render);
