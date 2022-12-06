f0ef7ec0d3d2e3a6b0cc5fb4770cbcd0de26907e
Cameron Alexander <emptyflash@gmail.com>
2022-12-05 22:23:12 -0500

    Add audio reactivity

--- main.js
+++ main.js
@@ -13,34 +13,63 @@ const height = canvas.height;
 const gpu = new GPU({canvas});
 let kernel;
 let previousTime;
+const audioCtx = new AudioContext();
+if (audioCtx.state === 'suspended') {
+    const unlock = () => {
+        audioCtx.resume().then(() => {
+                document.body.removeEventListener('touchstart', unlock);
+                document.body.removeEventListener('touchend', unlock);
+                document.body.removeEventListener('click', unlock);
+            });
+    };
+
+    document.body.addEventListener('touchstart', unlock, false);
+    document.body.addEventListener('touchend', unlock, false);
+    document.body.addEventListener('click', unlock, false);
+}
+const analyser = audioCtx.createAnalyser();
+analyser.fftSize = 32;
+navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    const source = audioCtx.createMediaStreamSource(stream);
+    source.connect(analyser);
+})
+const samples = analyser.frequencyBinCount;
+const audioData = new Uint8Array(samples);
+let audioTime = 0;
 
 function evalCode(code) {
-  const codeFn = Function("t", "p", `
-    let x = this.thread.x;
-    let y = this.thread.y;
-    let c = [-1,-1,-1,-1];
-    let o = -1;
-    ${code.indexOf("o =") >= 0 ? code : "o = " + code}
-    if (c[0] !== -1 && c[1] !== -1 && c[2] !== -1 && c[3] !== -1) {
-      this.color(c[0], c[1], c[2], c[3]);
-    } else {
-      o /= 255;
-      this.color(o, o, o, 1);
-    }
-  `);
-  let newKernel = gpu.createKernel(codeFn)
-    .setDebug(true)
-    .setOutput([width, height])
-    .setGraphical(true);
+  let newKernel;
   try {
-    newKernel(previousTime, canvas);
+    const codeFn = Function("t", "p", "w", "h", "a", "at", `
+      let x = this.thread.x;
+      let y = this.thread.y;
+      let c = [-1,-1,-1,-1];
+      let o = -1;
+      ${code.indexOf("o =") >= 0 ? code : "o = " + code}
+      if (c[0] !== -1 || c[1] !== -1 || c[2] !== -1 || c[3] !== -1) {
+        this.color(
+          c[0]==-1?0:c[0]/255,
+          c[1]==-1?0:c[1]/255,
+          c[2]==-1?0:c[2]/255,
+          c[3]==-1?0:c[3]/255,
+        );
+      } else {
+        o = o==-1?0:o/255;
+        this.color(o, o, o, 1);
+      }
+    `);
+    newKernel = gpu.createKernel(codeFn)
+      .setDebug(true)
+      .setOutput([width, height])
+      .setGraphical(true);
+    newKernel(previousTime, canvas, width, height, audioData, audioTime);
   } catch (err) {
     console.error(err);
     return true;
   }
   kernel = newKernel;
   params.set("c", code);
-  window.history.replaceState({}, '', `${location.pathname}?${params.toString()}`);
+  window.history.pushState({}, '', `${location.pathname}?${params.toString()}`);
   return true;
 }
 
@@ -78,8 +107,10 @@ runButton.onclick = runButtonFn
 function render(time) {
   previousTime = time;
   if (kernel) {
+    analyser.getByteFrequencyData(audioData );
+    audioTime = audioData.reduce((a,b) => a+b, 0)
     try {
-      kernel(time, canvas);
+      kernel(time, canvas, width, height, audioData, audioTime);
     } catch (err) {
       console.error(err);
     }
