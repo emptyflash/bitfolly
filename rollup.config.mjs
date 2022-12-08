0ef12b751b4aa03de54e72da2ef3a499bbcf2dc6
Cameron Alexander <emptyflash@gmail.com>
2022-12-08 10:11:49 -0500

    Remove livereload and rebundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,5 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve"
 import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
 
 export default {
   input: "./main.js",
@@ -10,7 +9,5 @@ export default {
   },
   plugins: [
     nodeResolve(),
-    serve({ contentBase: ".",  port: 8080 }),
-    livereload({ watch: "." })
   ],
 }
